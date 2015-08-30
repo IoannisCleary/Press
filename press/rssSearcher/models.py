fff69985803247d655e4b70862125076977ae1a0
IoannisCleary <nicolemaniac@gmail.com>
2015-08-31 00:59:53 +0300

    Layout - Template Update, ID field added to Entries
    - New layout added
    -- Entries are shown as expandable containers where the title is shown ,
    an expand button is on the right and date + time under the title.
    -- When expanded the description is shown in a well container and under
    it the source/URL is shown
    - Colours changed to blue/primary and dark blue title

--- press/rssSearcher/models.py
+++ press/rssSearcher/models.py
@@ -10,7 +10,8 @@ class Feed(models.Model):
 
 class Item(models.Model):
 	feed = models.ForeignKey(Feed)
-	title = models.CharField(max_length = 256, unique = True)
+	id = models.AutoField(primary_key=True)
+	title = models.CharField(max_length = 256, unique=True)
 	link = models.URLField()
 	description = models.CharField(max_length = 2000)
 	dateTime = models.CharField(max_length = 20, default="00:00 00/00/0000")
