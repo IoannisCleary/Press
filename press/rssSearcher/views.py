bd9b30d19cd559bb66419143ba43743d7f322b6e
IoannisCleary <nicolemaniac@gmail.com>
2015-08-30 15:37:18 +0300

    Initial steps for RSS feed parser view done
    In the views file, the view associated with the home page:
    - Retrieves or creates rss feed from a URL
    - It creates or retrieves all the items/entries in the database that are
    associated with that feed
    - Adds them all to a variable in the context_dict which will then be
    used to display on the page

--- press/rssSearcher/views.py
+++ press/rssSearcher/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-	
+from rssSearcher.models import Feed,Item
+import feedparser
+# feed : http://www.theguardian.com/world/rss
 def home(request):
 	context_dict = {}
+	source = feedparser.parse('http://www.theguardian.com/world/rss')
+	subject = source['feed']['title']
+	feed, _ = Feed.objects.get_or_create(subject=subject, link='http://www.theguardian.com/world/rss')
+	for i in range(0,len(source['entries'])):
+		print i
+		print source.entries[i]['title']
+		item, flag = Item.objects.get_or_create(feed=feed,title=source.entries[i]['title'], link=source.entries[i]['link'], description=source.entries[i]['description'])
+	items = Item.objects.filter(feed = feed)
+	context_dict['items'] = items
 	return render(request, 'rssSearcher/home.html', context_dict)
