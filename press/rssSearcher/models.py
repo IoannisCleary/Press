3fcd980a4451656d53196f3ea0907de0bedd443a
IoannisCleary <nicolemaniac@gmail.com>
2015-08-30 15:25:59 +0300

    Database model setup
    Models:
    - Feed : includes the subject of the feed and the url to the feed
    - Item : is a feed's entry and it has a title, link and description. In
    addition to a foreign key field that links it with the feed
    --
    Models registered on the admin page

--- press/rssSearcher/models.py
+++ press/rssSearcher/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Feed(models.Model):
+	subject = models.CharField(max_length = 128, unique = True)
+	link = models.URLField()
+	def __unicode__(self): 
+		return self.subject
+
+class Item(models.Model):
+	feed = models.ForeignKey(Feed)
+	title = models.CharField(max_length = 256, unique = True)
+	link = models.URLField()
+	description = models.CharField(max_length = 2000)
+
+	def __unicode__(self): 
+		return self.title
+	class Meta:
+		verbose_name_plural = "Items"
